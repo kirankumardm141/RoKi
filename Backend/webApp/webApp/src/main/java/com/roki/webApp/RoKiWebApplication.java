a560ae5347d00c511f5762dfdd7c6129cff5fcbe
Kiran Kumar D M <kirankumardm141@gmail.com>
2025-03-08 23:14:55 +0530

    Initial commit of RoKi web application

---
+++ Backend/webApp/webApp/src/main/java/com/roki/webApp/RoKiWebApplication.java
@@ -0,0 +1,13 @@
+package com.roki.webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoKiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoKiWebApplication.class, args);
+	}
+
+}
